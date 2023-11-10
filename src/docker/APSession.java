0cdcc2803c20416a10eb12a5be7fd4a5c44174f1
AndreaPunta <andrea.punta@esternibisp.com>
2023-11-10 23:06:49 +0100

    first commit

---
+++ src/docker/APSession.java
@@ -0,0 +1,13 @@
+package docker;
+
+import java.util.concurrent.TimeUnit;
+
+public class APSession {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Starting...");
+		TimeUnit.SECONDS.sleep(5);
+		System.out.println("Started");
+	}
+
+}
